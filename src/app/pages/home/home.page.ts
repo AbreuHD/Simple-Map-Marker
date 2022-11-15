cec01e45dcaebebaeb5f5a0186ead5605ca54c68
Jefferson Abreu Martinez <abreumartinezjefferson@gmail.com>
2022-11-15 15:15:01 -0400

    Leaflet Done

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as L from 'leaflet';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-
+export class HomePage{
+  map: any;
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([18.4511009, -69.6642079], 16.71);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
   }
 
 }
